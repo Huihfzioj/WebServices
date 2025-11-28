081de51694698fce93cdb7d79c5d88636d624831
Huihfzioj <hediljlassi64@gmail.com>
2025-11-28 22:22:09 +0100

    Add documentation

--- AdministrativeRequests/src/main/java/com/example/AdministrativeRequests/Model/AdminRequest.java
+++ AdministrativeRequests/src/main/java/com/example/AdministrativeRequests/Model/AdminRequest.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -43,7 +44,7 @@ public class AdminRequest {
     private String decisionReason;
 
     @ElementCollection
-    private List<String> attachments = new ArrayList<>();
+    private List<MultipartFile> attachments = new ArrayList<>();
 
     @PrePersist
     protected void onCreate() {

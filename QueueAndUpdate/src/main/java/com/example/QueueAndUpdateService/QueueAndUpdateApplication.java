f76491f7f74a4fa07cfef0a0eef34c665e09d4c3
Huihfzioj <hediljlassi64@gmail.com>
2025-12-04 16:05:48 +0100

    add queue service (tested)

--- QueueAndUpdate/src/main/java/com/example/QueueAndUpdateService/QueueAndUpdateApplication.java
+++ QueueAndUpdate/src/main/java/com/example/QueueAndUpdateService/QueueAndUpdateApplication.java
@@ -1,13 +1,11 @@
-package com.example;
+package com.example.QueueAndUpdateService;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueAndUpdateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QueueAndUpdateApplication.class, args);
     }
 }
